d678358f34d72b5850fab14257228c475962fdc2
tomorrowbye <tomorrowbye7@gmail.com>
2025-04-14 16:54:46 +0800

    feat(layout): refactor layout styles and theme switch

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,8 @@ import useRequest from "@/hooks/useRequest";
 import { getUserInfo } from "@/service/api";
 import { useUserStore } from "@/store";
 import "./globals.css";
+// import { ThemeProvider } from "@/components/theme/theme-provider/theme-provider";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
   children,
@@ -21,17 +23,19 @@ export default function RootLayout({
   });
 
   return (
-      <html lang="en">
+      <html suppressHydrationWarning lang="en"> 
         <body className="flex h-[100vh] w-[100vw] flex-col">
-          <Header />
-          {loading ? (
-            <div className="flex flex-1 items-center justify-center">
-              <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
-            </div>
-          ) : (
-            <main className="flex flex-1">{children}</main>
-          )}
-          <Toaster position="top-center" />
+          <ThemeProvider defaultTheme="system" storageKey="jasper|theme">
+            <Header className="fixed top-0 left-0 z-10" />
+            {loading ? (
+              <div className="flex flex-1 items-center justify-center">
+                <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
+              </div>
+            ) : (
+              <main className="flex flex-1">{children}</main>
+            )}
+            <Toaster position="top-center" />
+          </ThemeProvider>
         </body>
       </html>
   );
