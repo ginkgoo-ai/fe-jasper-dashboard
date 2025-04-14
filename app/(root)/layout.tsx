d3247fe912d9cd055fcaf66a2dfa5e2557451170
tomorrowbye <tomorrowbye7@gmail.com>
2025-04-14 16:55:47 +0800

    style(layout): remove comment

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,6 @@ import useRequest from "@/hooks/useRequest";
 import { getUserInfo } from "@/service/api";
 import { useUserStore } from "@/store";
 import "./globals.css";
-// import { ThemeProvider } from "@/components/theme/theme-provider/theme-provider";
 import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
@@ -23,20 +22,20 @@ export default function RootLayout({
   });
 
   return (
-      <html suppressHydrationWarning lang="en"> 
-        <body className="flex h-[100vh] w-[100vw] flex-col">
-          <ThemeProvider defaultTheme="system" storageKey="jasper|theme">
-            <Header className="fixed top-0 left-0 z-10" />
-            {loading ? (
-              <div className="flex flex-1 items-center justify-center">
-                <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
-              </div>
-            ) : (
-              <main className="flex flex-1">{children}</main>
-            )}
-            <Toaster position="top-center" />
-          </ThemeProvider>
-        </body>
-      </html>
+    <html suppressHydrationWarning lang="en"> 
+      <body className="flex h-[100vh] w-[100vw] flex-col">
+        <ThemeProvider defaultTheme="system" storageKey="jasper|theme">
+          <Header className="fixed top-0 left-0 z-10" />
+          {loading ? (
+            <div className="flex flex-1 items-center justify-center">
+              <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
+            </div>
+          ) : (
+            <main className="flex flex-1">{children}</main>
+          )}
+          <Toaster position="top-center" />
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
